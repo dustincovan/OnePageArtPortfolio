06729d39f16698322e6ebc947cb2a533c65d7801
Dustin Covan <dustincovan@users.noreply.github.com>
2015-11-02 14:28:44 -0500

    Create pictureBooks.js

---
+++ pictureBooks.js
@@ -0,0 +1,26 @@
+var pictureBooksHolder = "<div id='pictureBooksRow1' class='row'>"+
+                            "<div class='col-sm-12'>"+
+                              "<div class='pictureBook-container'>"+
+                                "<iframe src='http://online.fliphtml5.com/hlur/eurz/'  seamless='seamless' scrolling='no' frameborder='0' allowtransparency allowfullscreen></iframe>"+
+                              "</div>"+
+                            "</div>"+
+                          "</div>"+
+                          "<br>"+
+                          "<div id='pictureBooksRow2' class='row'>"+
+                            "<div class='col-sm-12'>"+
+                              "<div class='pictureBook-container'>"+
+                                "<iframe src='http://online.fliphtml5.com/hlur/knhe/'  seamless='seamless' scrolling='no' frameborder='0' allowtransparency allowfullscreen></iframe>"+
+                              "</div>"+
+                            "</div>"+
+                          "</div>"+
+                          "<br>"+
+                          "<div id='pictureBooksRow3' class='row'>"+
+                            "<div class='col-sm-12'>"+
+                              "<div class='pictureBook-container'>"+
+                                "<iframe src='http://online.fliphtml5.com/hlur/vcsi/' seamless='seamless' scrolling='no' frameborder='0' allowtransparency allowfullscreen></iframe>"+
+                              "</div>"+
+                            "</div>"+
+                          "</div>"+
+                          "<br>";
+
+document.getElementById('pictureBooks').innerHTML = pictureBooksHolder;
