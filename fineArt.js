eb70512d68116799f7c11beb51a2195977454d83
Dustin Covan <dustincovan@users.noreply.github.com>
2015-11-02 14:25:50 -0500

    Create fineArt.js

---
+++ fineArt.js
@@ -0,0 +1,74 @@
+
+var fineArtHolder = "<div id='links'>"+
+                      "<div id='fineArtRow1' class='row'>"+
+                      "<div class='col-sm-3'><a href='img/AndTheRedBig.png' title='And The Red' data-gallery><img src='img/AndTheRedThumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/RED-1.png' title='And The Red 1' data-gallery><img src='img/RED-1.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/RED-2.png' title='And The Red 2' data-gallery><img src='img/RED-2.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/RED-3.png' title='And The Red 3' data-gallery><img src='img/RED-3.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                    "</div>"+
+                    "<br>"+ 
+                    "<div id='fineArtRow2' class='row'>"+
+                      "<div class='col-sm-3'><a href='img/RED-4.png' title='And The Red 4' data-gallery><img src='img/RED-4.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/RED-5.png' title='And The Red 5' data-gallery><img src='img/RED-5.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/RED-6.png' title='And The Red 6' data-gallery><img src='img/RED-6.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/MayitaRascalita.png' title='Mayita Rascalita' data-gallery><img src='img/MayitaRascalitaThumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                    "</div>"+ 
+                    "<br>"+
+                    "<div id='fineArtRow3' class='row'>"+
+                      "<div class='col-sm-3'><a href='img/BeastKeepersBig.png' title='Beast Keepers' data-gallery><img src='img/BeastKeepersThumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/BeastKeepers-1.png' title='Beast Keepers 1' data-gallery><img src='img/BeastKeepers-1.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/BeastKeepers-2.png' title='Beast Keepers 2' data-gallery><img src='img/BeastKeepers-2.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/BeastKeepers-3.png' title='Beast Keepers 3' data-gallery><img src='img/BeastKeepers-3.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                    "</div>"+ 
+                    "<br>"+
+                    "<div id='fineArtRow4' class='row'>"+
+                      "<div class='col-sm-3'><a href='img/BeastKeepers-4.png' title='Beast Keepers 4' data-gallery><img src='img/BeastKeepers-4.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/BeastKeepers-5.png' title='Beast Keepers 5' data-gallery><img src='img/BeastKeepers-5.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/BeastKeepers-6.png' title='Beast Keepers 6' data-gallery><img src='img/BeastKeepers-6.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/Kidnappers.png' title='Kidnappers' data-gallery><img src='img/KidnappersThumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                    "</div>"+
+                    "<br>"+
+                    "<div id='fineArtRow5' class='row'>"+
+                      "<div class='col-sm-3'><a href='img/GreenAnd-1Big.png' title='And The Green And 1' data-gallery><img src='img/GreenAnd-1Thumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/GreenAnd-2Big.png' title='And The Green And 2' data-gallery><img src='img/GreenAnd-2Thumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/GreenAnd-3Big.png' title='And The Green And 3' data-gallery><img src='img/GreenAnd-3Thumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/GreenAnd-4Big.png' title='And The Green And 4' data-gallery><img src='img/GreenAnd-4Thumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                    "</div>"+
+                    "<br>"+
+                    "<div id='fineArtRow6' class='row'>"+
+                      "<div class='col-sm-3'><a href='img/GreenAnd-5Big.png' title='And The Green And 5' data-gallery><img src='img/GreenAnd-5Thumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/GreenAnd-6Big.png' title='And The Green And 6' data-gallery><img src='img/GreenAnd-6Thumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/AndTheGreatGoldenBeastBig.png' title='And The Great Golden Beast' data-gallery><img src='img/AndTheGreatGoldenBeastThumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/ATGGB-1.png' title='And The Great Golden Beast 1' data-gallery><img src='img/ATGGB-1Thumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                    "</div>"+
+                    "<br>"+
+                    "<div id='fineArtRow7' class='row'>"+
+                      "<div class='col-sm-3'><a href='img/ATGGB-2.png' title='And The Great Golden Beast 2' data-gallery><img src='img/ATGGB-2Thumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/ATGGB-3.png' title='And The Great Golden Beast 3' data-gallery><img src='img/ATGGB-3Thumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/ATGGB-4.png' title='And The Great Golden Beast 4' data-gallery><img src='img/ATGGB-4Thumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/ThreeOnAWormBig.png' title='Three On A Worm' data-gallery><img src='img/ThreeOnAWormThumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                    "</div>"+
+                    "<br>"+
+                    "<div id='fineArtRow8' class='row'>"+
+                      "<div class='col-sm-3'><a href='img/ToWooMagoonaBig.png' title='To Woo Magoona' data-gallery><img src='img/ToWooMagoonaThumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/RedWhiteAndBlackBig.png' title='Red White And Black' data-gallery><img src='img/RedWhiteAndBlackThumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/HugItBig.png' title='Hug It' data-gallery><img src='img/HugItThumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-3'><a href='img/marriageFull.png' title='Engaged' data-gallery><img src='img/marriageThumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<a href='img/marriageDetail.png' title='Engaged (detail)' data-gallery></a>"+
+                    "</div>"+
+                    "<br>"+
+                    "<div id='fineArtRow9' class='row'>"+
+                      "<div class='col-sm-4'><a href='img/spaceWorms1.png' title='Space Worms 1' data-gallery><img src='img/spaceWorms1Thumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-4'><a href='img/spaceWorms2.png' title='Space Worms 2' data-gallery><img src='img/spaceWorms2Thumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                      "<div class='col-sm-4'><a href='img/spaceWorms3.png' title='Space Worms 3' data-gallery><img src='img/spaceWorms3Thumbnail.png' class='img-responsive img-circle img-thumbnail'></a></div>"+
+                    "</div>"+
+                    "</div>"
+                    "<br>";
+
+document.getElementById('fineArt').innerHTML = fineArtHolder;
+
+$('.img-thumbnail').hover(function(){
+            $(this).css({'opacity': '0.8', 'border-color': 'yellow'});
+          }, function(){
+            $(this).css({'opacity': '1.0', 'border-color': '#00FFFF'});
+          });
