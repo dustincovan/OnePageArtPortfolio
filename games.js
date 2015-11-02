586fa772e2b5a40509096f3ddd80ece91d7930ba
Dustin Covan <dustincovan@users.noreply.github.com>
2015-11-02 14:28:06 -0500

    Create games.js

---
+++ games.js
@@ -0,0 +1,50 @@
+var gamesHolder= "<div id='gamesRow1' class='row'>"+
+                    "<div class='col-sm-7'>"+
+                      "<a href='http://www.kongregate.com/games/dustincovan/yeti-yeti-yeti-yeti' target='_blank'>"+
+                        "<img src='img/YetiGameScreen.png' class='img-responsive img-rounded img-thumbnail' id='yetiPicture'>"+
+                      "</a>"+
+                    "</div>"+
+                    "<div class='col-sm-5' id='yetiColumn'>"+
+                    "<br>"+
+                      "<div class='neonBlue' id='yetiBlurb'>"+
+                        "<p>"+
+                          "How do you play a game designed by The Enemy?"+
+                        "</p>"+
+                        "<p>"+
+                          "What do you do if the only tasks you can seemingly "+
+                          "accomplish are the ones where The Enemy wins?"+
+                        "</p>"+
+                        "<p>"+
+                          "Are they really the only options?"+ 
+                        "</p>"+
+                        "<p>"+
+                          "Is The Enemy really your enemy?"+
+                        "</p>"+
+                        "<p>"+
+                          "What kind of Yeti goes around trying to solve a "+
+                          "worm&#8217;s problems in the first place?"+
+                        "</p>"+
+                        "<p>"+
+                          "These are just some of the questions you may ask "+
+                          "yourself once you choose to experience Yeti Yeti Yeti "+
+                          "Yeti, by Dustin Covan."+
+                        "</p>"+
+                        "<p>"+
+                          "Like an episode of Teletubbies directed by David "+
+                          "Lynch, this interactive short story will reveal "+
+                          "itself to you in ways you may not be used to or "+
+                          "comfortable with; however, satisfaction is "+
+                          "guaranteed."+
+                        "</p>"+
+                        "<p>"+
+                          "(Here is a <a href="+
+                          "'http://www.kongregate.com/games/dustincovan%20/yeti-yeti-yeti-yeti'"+
+                             "class='pink' target='_blank'>link</a> to the game. Give it 5 stars if you like "+
+                             "it!)"+
+                        "</p>"+
+                      "</div>"+
+                    "</div>"+
+                  "</div> <!-- end gamesRow1 -->"+
+                  "<br>";
+
+document.getElementById('games').innerHTML = gamesHolder;
